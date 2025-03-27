44dab6bee5ff3093884c815739be1bf5baa004de
pen-duo <2411363896@qq.com>
2025-03-27 10:31:39 +0800

    feat: 完成登录功能

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  // 全局管道：用于请求参数的验证和转换
+  // transform: true 表示自动将请求数据转换为 DTO 类中定义的类型
+  // 例如：将字符串 "123" 转换为数字 123，将字符串日期转换为 Date 对象等
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { LoginUserDto } from './dto/login-user.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './entities/user.entity';
@@ -11,7 +12,7 @@ export class UserService {
     const users: User[] = await this.dbService.read();
     const foundUser = users.find((item) => item.username === registerUserDto.username);
     if (foundUser) {
-      throw new Error('该用户名已存在');
+      throw new BadRequestException('该用户名已存在');
     }
     const user = new User();
     user.username = registerUserDto.username;
@@ -19,5 +20,17 @@ export class UserService {
     users.push(user);
     await this.dbService.write(users);
     return users;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const users: User[] = await this.dbService.read();
+    const foundUser = users.find((item) => item.username === loginUserDto.username);
+    if (!foundUser) {
+      throw new BadRequestException('用户名用户不存在');
+    }
+    if (foundUser.password !== loginUserDto.password) {
+      throw new BadRequestException('密码不正确');
+    }
+    return foundUser;
   }
 }
